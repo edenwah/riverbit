1bcb31282495bf19c4057e2e227fdbb6deebb1b8
eden <eden.fung@outlook.com>
2025-09-25 06:34:00 +0100

    fix build bugs. added 404 page. changed routes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { fileURLToPath, URL } from 'url';
+
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        tailwindcss()
-    ],
-    base: '/',
+  base: '/riverbit/',
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL('index.html', import.meta.url)),
+        404: fileURLToPath(new URL('public/404.html', import.meta.url))
+      }
+    }
+  }
 });
