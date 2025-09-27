f4ee7ced3be02b9c8d3093a9f7e32e7f19fa03c4
edenwah <eden.fung@outlook.com>
2025-09-27 02:23:51 +0100

    remove cache footer

--- src/components/Footer.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-const Footer = () => {
-    return (_jsxs("div", { className: "flex flex-col self-stretch bg-zinc-900 py-6 px-4 gap-2", children: [_jsx("span", { className: "text-zinc-400 text-sm text-center sm:mx-4 xs:mx-2", children: "Lo-Fi v1.4 \u00B7 Based on PRD v1.4 \u00B7 Points System + Additional Modules + Insurance Pool | Arbitrum One Network | USDC Only" }), _jsx("span", { className: "text-zinc-400 text-sm text-center sm:mx-4 xs:mx-2", children: "Three-column Trading Layout | Liquidity Pools (Foundation / Main Pool / Insurance Pool) | Asset & Points Center | Full Modules" })] }));
-};
-export default Footer;
