5458a9ba6c0536b8bd1ffe949f1cde43ced1cfee
edenwah <eden.fung@outlook.com>
2025-09-27 04:18:46 +0100

    Updated vite.config.js base to ./

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'url';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/riverbit/',
+  base: './',
   plugins: [
     react(),
     tailwindcss()
