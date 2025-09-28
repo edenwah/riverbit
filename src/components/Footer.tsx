85ebc024a78a72dfae24feeb3b46711f76d7d3f3
edenwah <eden.fung@outlook.com>
2025-09-28 23:33:38 +0100

    updated footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="flex flex-col self-stretch bg-zinc-900 py-6 px-4 gap-2">
       <span className="text-zinc-400 text-sm text-center sm:mx-4 xs:mx-2">
-        Three-column Trading Layout | Liquidity Pools (Foundation / Main Pool / Insurance Pool) | Asset & Points Center | Full Modules
+      © RiverBit | All rights reserved
       </span>
     </div>
   );
